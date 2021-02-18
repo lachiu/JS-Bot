935d2712afa99d3024fa1c3ea6356bfdbf733399
lachiu <mathieu.van.daele@hotmail.com>
2021-02-18 07:08:32 +0100

    Im fucking up here

---
+++ index.js
@@ -0,0 +1,17 @@
+// Require these modules
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+
+// loads the .env
+dotenv.config();
+
+// create a new Discord client
+const bot = new Discord.Client();
+
+// once the bot is loaded it does this
+bot.once('ready', () => {
+	console.log('Ready!');
+});
+
+// the bot logs in
+bot.login(process.env.TOKEN);
